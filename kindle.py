deca057d64ce2351d276f2f88ac0e8237184a454
kimvais <kimvais@Kimmos-MacBook-Air.local>
2012-02-29 10:40:52 +0200

    PEP8fied

--- kindle.py
+++ kindle.py
@@ -1,7 +1,7 @@
 '''
 Copyright 2010 Kimmo Parviainen-Jalanko <k+kindle@77.fi>. All rights
 reserved.
-''' 
+'''
 
 import simplejson as json
 import glob
@@ -16,94 +16,109 @@ from ConfigParser import ConfigParser
 cp = ConfigParser()
 cp.read('kiehinen.conf')
 
-KINDLEDIR = cp.get('system','kindle_path')
+KINDLEDIR = cp.get('system', 'kindle_path')
 
 BOOKPATH = KINDLEDIR + "documents/"
 JSONFILE = KINDLEDIR + "system/collections.json"
 
 KINDLE_INTERNAL_PATH = "/mnt/us/documents/"
-COLLNAME = "%s@en-US" 
+COLLNAME = "%s@en-US"
 
 # JSON helper functions
+
+
 def load_data():
     try:
         return json.load(open(JSONFILE, 'r'))
     except IOError:
-        LOG(2,"File %s not found" % JSONFILE)
+        LOG(2, "File %s not found" % JSONFILE)
         return dict()
+
 
 def save_data(kjd):
     open(JSONFILE, 'w').write(json.dumps(kjd))
 
 # Kindle access
+
+
 def get_books(progress_func=lambda: None):
     ret = {}
-    files =  glob.glob("%s/*" % BOOKPATH)
-    for c,fn in enumerate(files):
+    files = glob.glob("%s/*" % BOOKPATH)
+    for c, fn in enumerate(files):
         progress_func(c)
-        LOG(3,"\n - Processing file #%d: %s" % (c,fn))
+        LOG(3, "\n - Processing file #%d: %s" % (c, fn))
         key = make_hash(fn.replace(BOOKPATH, KINDLE_INTERNAL_PATH))
         val = ebook.Book(fn)
         if val.is_a_book:
             ret[key] = val
     return ret
 
+
 def get_bookcount():
     return len(glob.glob("%s/*" % BOOKPATH))
 
 # Collection functions
+
+
 def update_ts(collection):
-    collection['lastAccess'] = int(time()*1000)
+    collection['lastAccess'] = int(time() * 1000)
+
 
 def add_collection(kjd, collection):
-    time_ms = int(time()*1000)    
+    time_ms = int(time() * 1000)
     new_item = {}
     cn = COLLNAME % collection
-    LOG(3,"Adding collection %s" % cn)
+    LOG(3, "Adding collection %s" % cn)
     if not ((cn) in kjd):
-        kjd[cn] = {'items':[],'lastAccess':time_ms}
+        kjd[cn] = {'items': [], 'lastAccess': time_ms}
     else:
-        LOG(1,"Collection %s already exists" % collection)
+        LOG(1, "Collection %s already exists" % collection)
         update_ts(kjd[cn])
+
 
 def delete_collection(kjd, collection):
     cn = COLLNAME % collection
-    LOG(3,"Deleting collection %s" % cn)
+    LOG(3, "Deleting collection %s" % cn)
     if cn in kjd:
-        del kjd[cn]    
+        del kjd[cn]
     else:
-        LOG(1,'Collection %s does not exist' % collection)
+        LOG(1, 'Collection %s does not exist' % collection)
 
 # Item functions
+
+
 def make_hash(s):
     '''
-    Calculates a SHA1 hash, prepended with * from filename prefixed 
+    Calculates a SHA1 hash, prepended with * from filename prefixed
     by the path to documents folder on Kindle 2.x and 3.0.x
     '''
     from hashlib import sha1
     return "*%s" % sha1(s).hexdigest()
 
+
 def add_item(kjd, collection, hash):
     cn = COLLNAME % collection
     if not ((cn) in kjd):
-        LOG(1,"Error. collection %s does not exist" % collection)
+        LOG(1, "Error. collection %s does not exist" % collection)
     else:
         if hash in kjd[cn]['items']:
-            LOG(1,"%s was already in %s" % (hash,cn))
+            LOG(1, "%s was already in %s" % (hash, cn))
         else:
             kjd[cn]['items'].append(hash)
             update_ts(kjd[cn])
 
+
 def remove_item(kjd, collection, hash):
     cn = COLLNAME % collection
     if not ((cn) in kjd):
-        LOG(1,"Error. collection %s does not exist" % collection)
+        LOG(1, "Error. collection %s does not exist" % collection)
     else:
         if hash not in kjd[cn]['items']:
             return
         else:
             kjd[cn]['items'].remove(hash)
             update_ts(kjd[cn])
+
 
 def get_books_in_collection(kjd, collection):
     return kjd[COLLNAME % collection]['items']
